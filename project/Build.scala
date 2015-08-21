36d7051a2f379770644fd350751e35d221660a4f
Maga Abdurakhmanov <maga@inn.eu>
2015-08-21 17:19:28 +0200

    + gracefull akka cluster shutdown

--- project/Build.scala
+++ project/Build.scala
@@ -39,12 +39,22 @@ object Build extends sbt.Build {
       addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersionRoot cross CrossVersion.full)
     )
 
-  lazy val `hyperbus-root` = project.in(file(".")) aggregate(servicebus, `hyperbus-annotations`, `hyperbus-inner`, hyperbus, `hyperbus-akka`, `servicebus-t-distributed-akka`, `hyperbus-cli`)
+  lazy val `hyperbus-root` = project.in(file(".")) aggregate(
+    servicebus,
+    `hyperbus-annotations`,
+    `hyperbus-inner`,
+    hyperbus,
+    `hyperbus-akka`,
+    `servicebus-t-distributed-akka`,
+    `servicebus-t-kafka`,
+    `hyperbus-cli`
+  )
   lazy val servicebus = project.in(file("servicebus"))
   lazy val `hyperbus-annotations` = project.in(file("hyperbus-annotations")) dependsOn servicebus
   lazy val `hyperbus-inner` = project.in(file("hyperbus-inner")) dependsOn (servicebus, `hyperbus-annotations`)
   lazy val hyperbus = project.in(file("hyperbus")) dependsOn `hyperbus-inner`
   lazy val `hyperbus-akka` = project.in(file("hyperbus-akka")) dependsOn hyperbus
   lazy val `servicebus-t-distributed-akka` = project.in(file("servicebus-t-distributed-akka")) dependsOn servicebus
+  lazy val `servicebus-t-kafka` = project.in(file("servicebus-t-kafka")) dependsOn servicebus
   lazy val `hyperbus-cli` = project.in(file("hyperbus-cli")) dependsOn (hyperbus, `servicebus-t-distributed-akka`)
 }
--- servicebus-t-distributed-akka/src/main/scala/eu/inn/servicebus/transport/ActorSystemRegistry.scala
+++ servicebus-t-distributed-akka/src/main/scala/eu/inn/servicebus/transport/ActorSystemRegistry.scala
@@ -2,11 +2,16 @@ package eu.inn.servicebus.transport
 
 import java.util.concurrent.atomic.AtomicInteger
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, ActorLogging, Actor, ActorSystem}
+import akka.cluster.ClusterEvent._
+import akka.cluster.{UniqueAddress, Cluster}
+import akka.contrib.pattern.DistributedPubSubExtension
+import akka.contrib.pattern.DistributedPubSubMediator.Publish
 import com.typesafe.config.{ConfigFactory, Config}
 import org.slf4j.LoggerFactory
 
 import scala.collection.concurrent.TrieMap
+import scala.concurrent.{Await, Promise}
 import scala.concurrent.duration.FiniteDuration
 
 object ActorSystemRegistry {
@@ -33,10 +38,8 @@ object ActorSystemRegistry {
   def release(actorSystemName: String)(implicit timeout: FiniteDuration) = {
     registry.get(actorSystemName) foreach { a ⇒
       if (a._2.decrementAndGet() <= 0) {
-        log.info(s"Shutting down ${a._1}")
         registry.remove(actorSystemName)
-        a._1.shutdown()
-        a._1.awaitTermination(timeout)
+        shutdownClusterNode(a._1)
       }
     }
   }
@@ -47,5 +50,45 @@ object ActorSystemRegistry {
     val as = ActorSystem(actorSystemName, akkaConfig)
     as.registerOnTermination(registry.remove(actorSystemName))
     as
+  }
+
+  def shutdownClusterNode(actorSystem: ActorSystem)(implicit timeout: FiniteDuration): Unit = {
+    val exitPromise = Promise[Boolean]()
+    val cluster = Cluster(actorSystem)
+    val me = cluster.selfUniqueAddress
+    val exitEventListener = actorSystem.actorOf(Props(new ExitEventListener(exitPromise, me)))
+    log.info(s"Leaving cluster $me...")
+    cluster.leave(me.address)
+
+    val result = Await.result(exitPromise.future, timeout/2)
+    if (!result)
+      log.warn(s"Didn't get confirmation that node left: $me")
+
+    log.info(s"Shutting down $actorSystem...")
+    actorSystem.shutdown()
+    actorSystem.awaitTermination(timeout)
+  }
+
+  class ExitEventListener(val exited: Promise[Boolean], nodeAddress: UniqueAddress) extends Actor with ActorLogging {
+    val cluster = Cluster(context.system)
+
+    override def preStart(): Unit = {
+      cluster.subscribe(self, initialStateMode = InitialStateAsEvents, classOf[ClusterDomainEvent])
+    }
+
+    override def postStop(): Unit = {
+      cluster.unsubscribe(self)
+      if (!exited.isCompleted)
+        exited.success(false)
+    }
+
+    def receive = {
+      case e: MemberExited ⇒
+        if (e.member.uniqueAddress == nodeAddress) {
+          exited.success(true)
+          log.info(s"Node exited: ${e.member.uniqueAddress}")
+        }
+      case _ ⇒ //ignore
+    }
   }
 }
