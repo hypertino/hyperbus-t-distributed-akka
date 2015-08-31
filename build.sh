fc74d03efa482302a4a5391ef48b6c2cc42d1930
Maga Abdurakhmanov <maga@inn.eu>
2015-08-31 14:53:11 +0200

    * fixed publish script

--- build.sh
+++ build.sh
@@ -13,5 +13,14 @@ for param in "$@"
 done
 
 if [ -n "$publish" ] ; then
-	sbt 'set every projectBuildNumber := "'${patch_version:-SNAPSHOT}'"' 'set testOptions += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test-reports")' clean test hyperbus-transport/publish hyperbus-annotations/publish hyperbus-model/publish hyperbus/publish hyperbus-akka/publish hyperbus-t-distributed-akka/publish hyperbus-t-kafka/publish hyperbus-cli/publish
+	sbt 'set every projectBuildNumber := "'${patch_version:-SNAPSHOT}'"' 'set testOptions += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test-reports")' clean test \
+		hyperbus-transport/publish \
+		hyperbus-model/publish \
+		hyperbus-standard-model/publish \
+		hyperbus/publish \
+		hyperbus-t-inproc/publish \
+		hyperbus-akka/publish \
+		hyperbus-t-distributed-akka/publish \
+		hyperbus-t-kafka/publish \		
+		hyperbus-cli/publish
 fi
