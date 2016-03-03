4e575e052a0c857c1097f3d5e946b77d952c7993
Maga Abdurakhmanov <maga@inn.eu>
2016-03-03 23:46:15 +0100

    + generated id's are sorted by creation time

--- hyperbus-transport/src/main/scala/eu/inn/hyperbus/IdGenerator.scala
+++ hyperbus-transport/src/main/scala/eu/inn/hyperbus/IdGenerator.scala
@@ -5,25 +5,25 @@ import java.util.concurrent.atomic.AtomicInteger
 
 object IdGenerator {
   private val random = new SecureRandom()
-  private val base64t = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-~"
-  private val counter = new AtomicInteger(random.nextInt())
+  private val base64t = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~" // sorted by char code
+  private val counter = new AtomicInteger(random.nextInt(65536))
 
   def create(): String = {
     val sb = new StringBuilder(30)
     appendInt(sb, (System.currentTimeMillis() / 10000l & 0xFFFFFFFFl).toInt)
-    appendInt(sb, random.nextInt())
-    appendInt(sb, random.nextInt())
-    appendInt(sb, random.nextInt())
-    appendInt(sb, random.nextInt())
     appendInt(sb, counter.incrementAndGet())
+    appendInt(sb, random.nextInt())
+    appendInt(sb, random.nextInt())
+    appendInt(sb, random.nextInt())
+    appendInt(sb, random.nextInt())
     sb.toString()
   }
 
   private def appendInt(sb: StringBuilder, i: Int): Unit = {
-    sb.append(base64t.charAt(i & 63))
-    sb.append(base64t.charAt(i >> 6 & 63))
-    sb.append(base64t.charAt(i >> 12 & 63))
-    sb.append(base64t.charAt(i >> 18 & 63))
     sb.append(base64t.charAt(i >> 24 & 63))
+    sb.append(base64t.charAt(i >> 18 & 63))
+    sb.append(base64t.charAt(i >> 12 & 63))
+    sb.append(base64t.charAt(i >> 6 & 63))
+    sb.append(base64t.charAt(i & 63))
   }
 }
---
+++ hyperbus-transport/src/test/scala/eu/inn/hyperbus/transport/IdGeneratorSpec.scala
@@ -0,0 +1,18 @@
+package eu.inn.hyperbus.transport
+
+import eu.inn.hyperbus.IdGenerator
+import org.scalatest.{Matchers, FreeSpec}
+
+class IdGeneratorSpec extends FreeSpec with Matchers {
+  "IdGenerator " - {
+    "Should generate sorted sequence" in {
+
+      val list = 0 until 5000 map { i ⇒ IdGenerator.create() }
+      list.sortBy(l ⇒ l) should equal(list) // sequence is sorted
+      list.foreach { l ⇒
+        list.count(_ == l) should equal(1) //evey one is unique
+        l.length == 30
+      }
+    }
+  }
+}
