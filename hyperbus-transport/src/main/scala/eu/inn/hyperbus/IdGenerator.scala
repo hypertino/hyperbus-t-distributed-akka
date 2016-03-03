50542a24b8df716f63b78f32dcff20f5922bdf27
Maga Abdurakhmanov <maga@inn.eu>
2016-03-03 22:42:22 +0100

    + more chars & time sorted Id's, minimized collision possibility when generating sequentially

--- hyperbus-transport/src/main/scala/eu/inn/hyperbus/IdGenerator.scala
+++ hyperbus-transport/src/main/scala/eu/inn/hyperbus/IdGenerator.scala
@@ -1,23 +1,25 @@
 package eu.inn.hyperbus
 
 import java.security.SecureRandom
+import java.util.concurrent.atomic.AtomicInteger
 
 object IdGenerator {
   private val random = new SecureRandom()
   private val base64t = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-~"
+  private val counter = new AtomicInteger(random.nextInt())
 
   def create(): String = {
-    val sb = new StringBuilder(25)
-    next(sb)
-    next(sb)
-    next(sb)
-    next(sb)
-    next(sb)
+    val sb = new StringBuilder(30)
+    appendInt(sb, (System.currentTimeMillis() / 10000l & 0xFFFFFFFFl).toInt)
+    appendInt(sb, random.nextInt())
+    appendInt(sb, random.nextInt())
+    appendInt(sb, random.nextInt())
+    appendInt(sb, random.nextInt())
+    appendInt(sb, counter.incrementAndGet())
     sb.toString()
   }
 
-  private def next(sb: StringBuilder): Unit = {
-    val i = random.nextInt()
+  private def appendInt(sb: StringBuilder, i: Int): Unit = {
     sb.append(base64t.charAt(i & 63))
     sb.append(base64t.charAt(i >> 6 & 63))
     sb.append(base64t.charAt(i >> 12 & 63))
